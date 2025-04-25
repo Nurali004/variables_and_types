5b50a66ab818b615f5be015b0da64f0718bdb42e
M A C W I N <mavzurovnurali@gmail.com>
2025-04-25 08:48:41 +0500

    sss

--- begin01.py
+++ begin01.py
@@ -1,5 +1,9 @@
 # Assign 5 (int value) to 'num_one' variable;
+num_one=4
 
 # Assign 4 (int value) to 'num_two' variable;
+num_two=5
 
 # Print sum of 'num_one' and 'num_two;
+x=num_one+num_two
+print(x)
